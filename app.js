fc891c2986b6294526a4881c92846117b2a78bee
Haiyang Hong <hong961127@gmail.com>
2020-08-06 06:54:01 -0700

    Setup mongodb

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require("dotenv").config({ path: "./config/config.env" });
+require("./config/db")();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+
+const schema = require("./graphql/schema");
+const rootValue = require("./graphql/queries/index");
 
 const app = express();
 app.use(
   "/graphql",
   graphqlHTTP((req, res, gql) => ({
-    schema: null,
-    rootValue: null,
+    schema,
+    rootValue,
     graphiql: true,
     pretty: true,
   }))
